87c0f649eaddc775e901613556b0fd1c02babb99
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2023-04-25 23:25:56 -0300

    tests solo bin2de y dec2bin

--- test_sistnumeracion.py
+++ test_sistnumeracion.py
@@ -1,14 +1,21 @@
 import unittest
-from bin2dec_dec2bin from decimal2binario
-from bin2dec_dec2bin from binario2decimal
+from bin2dec_dec2bin import decimal2binario
+from bin2dec_dec2bin import binario2decimal
 
 class TestNumeracion(unittest.TestCase):
     def test_binario2decimal(self):
         self.assertEqual(binario2decimal('01011100'),92)
+    def test_binario2decimal2(self):
+        self.assertEqual(binario2decimal('10000000'),128)
+    def test_binario2decimal3(self):
+        self.assertEqual(binario2decimal('0111'),7)
     def test_decimal2binario(self):
         self.assertEqual(decimal2binario(97), '01100001')
-    def test_decimal2binario(self):
-        self.assertEqual(binario2decimal(120),'01111000')
+    def test_decimal2binario2(self):
+        self.assertEqual(decimal2binario(120),'01111000')
+    def test_decimal2binario3(self):
+        self.assertEqual(decimal2binario(4),'0100')
 
 if __name__=='__main__':
     unittest.main()
+    
